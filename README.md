d583328470d24ad6d4a8f24952bb252d57a29cf2
MattBussing <mbussing@mymail.mines.edu>
2019-11-29 21:36:43 -0700

    made make-output

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ Place this folder "OS-Project-2-Verifier" inside Project-2.
 Run from inside Project-2-YOURNAME folder.
 
 Create Pull Requests to help this get better.
+
+## For Example
+
+To run the tests
+
+`[matt@violet project-2-MattBussing]$ bash OS-Project-2-Verifier/test.sh`
+
+If my outputs are bad, consider using this to generate your own outputs
+
+`[matt@violet project-2-MattBussing]$ bash OS-Project-2-Verifier/make-output.sh`
--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+base_dir=OS-Project-2-Verifier/outputs
+
 echo "1 of 3 clean ======================"
 # make clean
 echo "2 of 3 unit tests ================="
@@ -5,19 +7,19 @@ make test
 echo "3 of 3 diff ======================="
 make
 
-printf "  Checking no flag output\n"
-diff -sw <(./mem-sim inputs/sim_1) verify/outputs/deliverable_02/sim_1_output_no_flag
+printf "===Checking no flag output\n"
+diff -sw <(./mem-sim inputs/sim_1) $base_dir/sim_1_output_no_flag
 
-printf "  Checking verbose flag output\n"
-diff -sw <(./mem-sim -v inputs/sim_1) verify/outputs/deliverable_02/sim_1_output_v_flag
+printf "===Checking verbose flag output\n"
+diff -sw <(./mem-sim -v inputs/sim_1) $base_dir/sim_1_output_v_flag
 
-printf "  Checking csv flag output\n"
-diff -sw <(./mem-sim -c inputs/sim_1) verify/outputs/deliverable_02/sim_1_output_c_flag
+printf "===Checking csv flag output\n"
+diff -sw <(./mem-sim -c inputs/sim_1) $base_dir/sim_1_output_c_flag
 
-printf "  Checking LRU output\n"
-diff -sw <(./mem-sim -s LRU inputs/sim_1) verify/outputs/deliverable_02/sim_1_output_LRU
+printf "===Checking LRU output\n"
+diff -sw <(./mem-sim -s LRU inputs/sim_1) $base_dir/sim_1_output_LRU
 
-printf "  Checking maxframes 11 output\n"
-diff -sw <(./mem-sim -vf 11 inputs/sim_1) verify/outputs/deliverable_02/sim_1_output_MF_11
+printf "===Checking maxframes 11 output\n"
+diff -sw <(./mem-sim -vf 11 inputs/sim_1) $base_dir/sim_1_output_MF_11
 
 echo "COMPLETED"
